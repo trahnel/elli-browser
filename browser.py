b92dc5ef8618b6c8c3ec798bce7cb62beeab8fa7
Taavi Rahnel <taavi@spotify.com>
2022-01-06 12:01:49 +0100

    Handle browser styles

--- browser.py
+++ browser.py
@@ -220,39 +220,21 @@ def layout_mode(node):
     return 'block'
 
 
-class BlockLayout:
-    def __init__(self, node, parent, previous):
-        self.node = node
-        self.parent = parent
-        self.children = []
-        self.previous = previous
+def style(node, rules):
+    node.style = {}
 
-    def layout(self):
-        previous = None
-        for child in self.node.children:
-            if layout_mode(child) == 'inline':
-                next_block = InlineLayout(child, self, previous)
-            else:
-                next_block = BlockLayout(child, self, previous)
-            self.children.append(next_block)
-            previous = next_block
+    for selector, body in rules:
+        if not selector.matches(node):
+            continue
+        for prop, value in body.items():
+            node.style[prop] = value
 
-        self.width = self.parent.width
-        self.x = self.parent.x
-
-        if self.previous:
-            self.y = self.previous.y + self.previous.height
-        else:
-            self.y = self.parent.y
-
-        for child in self.children:
-            child.layout()
-
-        self.height = sum([child.height for child in self.children])
-
-    def paint(self, display_list):
-        for child in self.children:
-            child.paint(display_list)
+    if isinstance(node, Element) and "style" in node.attributes:
+        pairs = CSSParser(node.attributes["style"]).body()
+        for prop, value in pairs.items():
+            node.style[prop] = value
+    for child in node.children:
+        style(child, rules)
 
 
 class InlineLayout:
@@ -353,11 +335,51 @@ class InlineLayout:
         self.cursor_y = baseline + 1.25 * max_descent
 
     def paint(self, display_list):
-        if isinstance(self.node, Element) and self.node.tag == 'pre':
+        bgcolor = self.node.style.get("background-color", "transparent")
+        if bgcolor != 'transparent':
             x2, y2 = self.x + self.width, self.y + self.height
-            display_list.append(DrawRect(self.x, self.y, x2, y2, 'gray'))
+            rect = DrawRect(self.x, self.y, x2, y2, bgcolor)
+            display_list.append(rect)
+        # if isinstance(self.node, Element) and self.node.tag == 'pre':
+        #     x2, y2 = self.x + self.width, self.y + self.height
+        # display_list.append(DrawRect(self.x, self.y, x2, y2, 'gray'))
         for x, y, word, font in self.display_list:
             display_list.append(DrawText(x, y, word, font))
+
+
+class BlockLayout:
+    def __init__(self, node, parent, previous):
+        self.node = node
+        self.parent = parent
+        self.children = []
+        self.previous = previous
+
+    def layout(self):
+        previous = None
+        for child in self.node.children:
+            if layout_mode(child) == 'inline':
+                next_block = InlineLayout(child, self, previous)
+            else:
+                next_block = BlockLayout(child, self, previous)
+            self.children.append(next_block)
+            previous = next_block
+
+        self.width = self.parent.width
+        self.x = self.parent.x
+
+        if self.previous:
+            self.y = self.previous.y + self.previous.height
+        else:
+            self.y = self.parent.y
+
+        for child in self.children:
+            child.layout()
+
+        self.height = sum([child.height for child in self.children])
+
+    def paint(self, display_list):
+        for child in self.children:
+            child.paint(display_list)
 
 
 class DocumentLayout:
@@ -414,6 +436,116 @@ class DrawRect:
         )
 
 
+class CSSParser:
+    def __init__(self, s: str):
+        self.s = s
+        self.i = 0
+
+    def whitespace(self):
+        while self.i < len(self.s) and self.s[self.i].isspace():
+            self.i += 1
+
+    def word(self):
+        start = self.i
+        while self.i < len(self.s):
+            if self.s[self.i].isalnum() or self.s[self.i] in "#-.%":
+                self.i += 1
+            else:
+                break
+        assert self.i > start
+        return self.s[start:self.i]
+
+    def literal(self, literal: str):
+        assert self.i < len(self.s) and self.s[self.i] == literal
+        self.i += 1
+
+    def pair(self):
+        prop = self.word()
+        self.whitespace()
+        self.literal(':')
+        self.whitespace()
+        val = self.word()
+        return prop.lower(), val
+
+    def body(self):
+        pairs = {}
+        while self.i < len(self.s) and self.s[self.i] != "}":
+            try:
+                prop, val = self.pair()
+                pairs[prop.lower()] = val
+                self.whitespace()
+                self.literal(';')
+                self.whitespace()
+            except AssertionError:
+                why = self.ignore_until([";", "}"])
+                if why == ";":
+                    self.literal(";")
+                    self.whitespace()
+                else:
+                    break
+        return pairs
+
+    def ignore_until(self, chars: str):
+        while self.i < len(self.s):
+            if self.s[self.i] in chars:
+                return self.s[self.i]
+            else:
+                self.i += 1
+
+    def selector(self):
+        out = TagSelector(self.word().lower())
+        self.whitespace()
+        while self.i < len(self.s) and self.s[self.i] != "{":
+            tag = self.word()
+            descendant = TagSelector(tag.lower())
+            out = DescendantSelector(out, descendant)
+            self.whitespace()
+        return out
+
+    def parse(self):
+        rules = []
+        while self.i < len(self.s):
+            try:
+                self.whitespace()
+                selector = self.selector()
+                self.literal("{")
+                self.whitespace()
+                body = self.body()
+                self.literal("}")
+                rules.append((selector, body))
+            except AssertionError:
+                why = self.ignore_until(["}"])
+                if why == "}":
+                    self.literal("}")
+                    self.whitespace()
+                else:
+                    break
+        return rules
+
+
+class TagSelector:
+    def __init__(self, tag):
+        self.tag = tag
+
+    def matches(self, node):
+        return isinstance(node, Element) and self.tag == node.tag
+
+
+class DescendantSelector:
+    def __init__(self, ancestor, descendant):
+        self.ancestor = ancestor
+        self.descendant = descendant
+
+    def matches(self, node):
+        if not self.descendant.matches(node):
+            return False
+        while node.parent:
+            if self.ancestor.matches(node.parent):
+                return True
+            node = node.parent
+        return False
+
+
 class Browser:
     def __init__(self):
         self.window = tkinter.Tk()
@@ -427,12 +559,28 @@ class Browser:
 
         self.display_list = []
 
+        with open("browser.css") as f:
+            self.default_style_sheet = CSSParser(f.read()).parse()
+
     def load(self, url):
+        # Requuest
         headers, body = request(url)
+
+        # DOM tree
         self.nodes = HTMLParser(body).parse()
         # print_tree(self.nodes)
+
+        # Layout tree init
         self.document = DocumentLayout(self.nodes)
+
+        # Styles
+        rules = self.default_style_sheet.copy()
+        style(self.nodes, rules)
+
+        # Create layout tree
         self.document.layout()
+
+        # Paint
         self.display_list = []
         self.document.paint(self.display_list)
         self.draw()
@@ -468,7 +616,7 @@ class Browser:
 
 if __name__ == "__main__":
     import sys
-    # Browser().load('http://localhost:8000/')
+    Browser().load('http://localhost:8000/')
     # Browser().load('https://www.zggdwx.com/xiyou/1.html')
-    Browser().load(sys.argv[1])
+    # Browser().load(sys.argv[1])
     tkinter.mainloop()
